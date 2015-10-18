05095326a90318d2d2f9cfb0648d50fd34d12b8f
Ruslan Shakirov <r.shakirov@parcsis.org>
2015-10-18 22:35:17 +0400

    задания

--- issue #8/static/js/main.js
+++ issue #8/static/js/main.js
@@ -1,9 +1,18 @@
 $(function() {
+	var data = {
+		login: '',
+		password: ''
+	};
+
+	$('#login').focusout(function(e){
+		data.login = e.target.value;
+	});
+	$('#password').focusout(function(e){
+		data.password = e.target.value;
+	});
+
 	$('.b-button').on('click', function() {
-		$.post('/login', {
-			login: $('.b-input[name="login"]').val(),
-			password: $('.b-input[name="password"]').val()
-		}, function() {
+		$.post('/login', data, function() {
 			alert('Авторизация прошла успешно!');
 		});
 	});
